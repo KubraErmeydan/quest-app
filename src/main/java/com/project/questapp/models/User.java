f6ce46ce209c9fdc46d3b599bda2ca10d1a9d9b7
Emine Tekcan <eminetek68can@gmail.com>
2023-05-17 23:20:30 +0300

    crated User Model

--- src/main/java/com/project/questapp/models/User.java
+++ src/main/java/com/project/questapp/models/User.java
@@ -1,6 +1,18 @@
 package com.project.questapp.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Table(name = "users")
+@Data
 public class User {
-    private Long user_id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private int avatar;
 }
