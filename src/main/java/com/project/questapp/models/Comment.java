14905c58c26a066450a92b986a43c5d15694514c
KubraErmeydan <73437863+KubraErmeydan@users.noreply.github.com>
2023-05-17 23:43:07 +0300

    like

---
+++ src/main/java/com/project/questapp/models/Comment.java
@@ -0,0 +1,38 @@
+package com.project.questapp.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "comment")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable= false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+
+    @Lob
+    @Column(columnDefinition = "text")
+    String text;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    Date createDate;
+
+}
