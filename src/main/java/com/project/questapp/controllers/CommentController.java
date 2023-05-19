ffbbbf3ac7fb28c2ae4be91cc75d4141f6e68070
KubraErmeydan <73437863+KubraErmeydan@users.noreply.github.com>
2023-05-19 23:34:49 +0300

    comment controller updated

--- src/main/java/com/project/questapp/controllers/CommentController.java
+++ src/main/java/com/project/questapp/controllers/CommentController.java
@@ -1,9 +1,36 @@
 package com.project.questapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.questapp.models.Comment;
+import com.project.questapp.repos.CommentRepository;
+import jakarta.persistence.AssociationOverride;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+
+    @Autowired
+    public CommentRepository commentRepository;
+
+    @GetMapping
+    public List<Comment> getAllComments(){
+        return commentRepository.findAll();
+    }
+    @PostMapping
+    public Comment createOneComment(@RequestBody Comment comment){
+        return commentRepository.save(comment);
+    }
+
+
+    @PutMapping("/{commentId}")
+    public Comment updateOneComment(@PathVariable Long commentId,@RequestBody Comment comment ){
+        return commentRepository.save(comment);
+    }
+    @DeleteMapping("/{commentId}")
+    public void deleteOneComment(@PathVariable Long commentId){
+        commentRepository.deleteById(commentId);
+    }
 }
